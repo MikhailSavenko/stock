69c1b23a686287a317d21dfd8ec30f444f1b3cbb
Михаил Савенко <mi47sav@gmail.com>
2024-09-21 15:18:52 +0300

    Отношения добавлены в Order и Product

--- app/models/order.py
+++ app/models/order.py
@@ -1,7 +1,9 @@
+from typing import List
 from sqlalchemy import DateTime, Enum
 from app.core.db import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, timezone
+from app.models.order_item import OrderItem
 from app.services.enums import Status
 
 
@@ -9,3 +11,5 @@ class Order(Base):
     """Модель Заказа"""
     create_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
     status: Mapped[Status] = mapped_column(Enum(Status), default=Status.IN_PROCESS)
+
+    order_item: Mapped[List['OrderItem']] = relationship(back_populates='order')
--- app/models/product.py
+++ app/models/product.py
@@ -1,11 +1,16 @@
+from typing import List
 from sqlalchemy import String, Text, Numeric, Integer
 from app.core.db import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.order_item import OrderItem
 
 
 class Product(Base):
     """Модель товара"""
     name: Mapped[str] = mapped_column(String(50))
-    discription: Mapped[str] = mapped_column(Text)
+    description: Mapped[str] = mapped_column(Text)
     cost: Mapped[float] = mapped_column(Numeric(10, 2))
     quantity: Mapped[int] = mapped_column(Integer)
+
+    order_item: Mapped[List['OrderItem']] = relationship(back_populates='product')
