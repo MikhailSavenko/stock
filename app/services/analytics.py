422a2c11a220bb645fc1e69746368f852e89de8f
Mikhail <misha47savenko@yandex.ru>
2026-07-08 22:06:29 +0300

    rewrite payload YandexMetricaClient

--- app/services/analytics.py
+++ app/services/analytics.py
@@ -1,4 +1,3 @@
-from typing import Optional, Any, Dict
 import logging
 import asyncio
 
@@ -7,28 +6,29 @@ logger = logging.getLogger("yandex_metrica")
 from app.core.celery_app import celery_app
 
 class YandexMetricaClient:
-    """Клиент для интеграции с Yandex Metrica API (Server-Side Tracking)"""
-    def __init__(self, counter_id: str = "12345678") -> None:
-        self.counter_id = counter_id
+    """Клиент для интеграции с Yandex Measurement Protocol"""
+    def __init__(self, tid: str = "Young&Junior") -> None:
+        self.tid = tid
 
     
     async def send_click_event(
-            self, client_id: str, target_name: str, user_params: Optional[Dict[str, Any]] = None
-    ) -> None:
+            self, client_id: str, order_id: int, price: float) -> None:
         payload = {
-            "counter_id": self.counter_id,
-            "client_id": client_id,
-            "target": target_name,
-            "user_params": user_params or {}
+            "tid": self.tid,
+            "cid": client_id,
+            "t": "event",
+            "pa": "purchase",
+            "ti": str(order_id),
+            "tr": price,
+            "cu": "RUB"
         }
-
         await asyncio.sleep(0.05)
-        logger.info(f"[Yandex Metrica Server-Side] Event sent successfully: {payload}")
+        logger.info(f"[Yandex Measurement Protocol] Event sent successfully: {payload}")
 
 metrica_client = YandexMetricaClient()
 
 
 @celery_app.task(name="app.services.analytics.send_metrica_task")
-def send_metrica_task(client_id: str, target_name: str, user_params: Dict[str, Any]) -> str:
-    asyncio.run(metrica_client.send_click_event(client_id, target_name, user_params))
+def send_metrica_task(client_id: str, order_id: int, price: float) -> str:
+    asyncio.run(metrica_client.send_click_event(client_id, order_id, price))
     return "Success"
